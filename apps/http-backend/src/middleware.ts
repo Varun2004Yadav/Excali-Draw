1031cc8092731bb998b9323f53b3d692a156a6c2
Varun2004Yadav <varun2004kkr@gmail.com>
2025-07-30 14:35:49 +0530

    First Commit

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "./config";
+
+
+export function middleware(req: Request, res: Response , next : NextFunction) {
+//@ts-ignore
+    const token = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(token,JWT_SECRET)
+    
+    if(decoded){
+        //@ts-ignore
+        req.userId = decoded.userId;
+    }else{
+        //@ts-ignore
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+}
